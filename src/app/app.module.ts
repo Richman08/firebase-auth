256c62384628a59a492902ad8358f7f49e722643
Oleg <oloktionov1985@gmail.com>
2019-12-05 19:44:09 +0200

    (dev): build structure, add onpush strategy, add home, header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from './shared/modules/material.module';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/modules/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+
+  ]
+})
+export class MaterialModule { }
