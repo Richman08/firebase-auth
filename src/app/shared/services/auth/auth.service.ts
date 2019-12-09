20c5bac288234c81ddf0f90fa96863c52b2f9ccc
Oleg <oloktionov1985@gmail.com>
2019-12-09 21:52:09 +0200

    (dev): add auth service

---
+++ src/app/shared/services/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {IUser} from '../../interfaces/user.interface';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Router} from '@angular/router';
+import {auth} from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  user$: Observable<IUser>;
+
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+              private router: Router) {
+    this.user$ = this.afAuth.authState;
+  }
+
+  async googleSignin() {
+    const provider = new auth.GoogleAuthProvider();
+    await this.afAuth.auth.signInWithPopup(provider);
+  }
+
+  async signOut() {
+    await this.afAuth.auth.signOut();
+    return this.router.navigate(['/']);
+  }
+}
